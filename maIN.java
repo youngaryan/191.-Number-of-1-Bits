4543d4043b87bfae0b94d14f64ede3135271aa02
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-05-17 20:46:52 +0100

    done

---
+++ maIN.java
@@ -0,0 +1,8 @@
+public class maIN {
+    public static void main(String[] args) {
+
+        Solution s = new Solution();
+
+        System.out.println(s.hammingWeight(00000000000000000000000010000000));
+    }
+}
